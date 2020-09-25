f42c48110c07cef56ba3c139b661cc307e4421d2
snake0717 <69613407+snake0717@users.noreply.github.com>
2020-09-25 18:30:10 +0900

    検索機能の実装

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,5 +1,5 @@
 class TweetsController < ApplicationController
-  before_action :move_to_index, except: [:index, :show]
+  before_action :move_to_index, except: [:index, :show, :serch]
   def index
     @tweets = Tweet.includes(:user).order("created_at DESC")
   end
@@ -30,6 +30,10 @@ class TweetsController < ApplicationController
     @tweet = Tweet.find(params[:id])
     @comment = Comment.new
     @comments = @tweet.comments.includes(:user)
+  end
+
+  def search
+    @tweets = Tweet.search(params[:keyword])
   end
 
 
