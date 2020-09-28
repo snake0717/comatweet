4f9b760278425226a32eb8ce4f42d37e7e0d1ff1
snake0717 <69613407+snake0717@users.noreply.github.com>
2020-09-28 17:20:39 +0900

    いいね機能の実装

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -2,6 +2,9 @@ class Tweet < ApplicationRecord
   validates :text, presence: true
   belongs_to :user
   has_many :comments
+  has_many :likes
+  has_many :like_users, through: :likes, source: :user
+
 
   def self.search(search)
     if search != ""
