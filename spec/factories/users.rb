34dfc01a8171e881f22e135bc56b2a86175465d6
snake0717 <69613407+snake0717@users.noreply.github.com>
2020-10-06 18:29:28 +0900

    ユーザー登録

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,9 @@
 FactoryBot.define do
   factory :user do
-    nickname              {"taro"}
-    email                 {"kkk@gmail.com"}
-    password              {"123456"}
-    password_confirmation {password}
+    nickname              { 'タナカ' }
+    email                 { 'test@test' }
+    password              { 'test123' }
+    password_confirmation { password }
+    
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,4 +5,26 @@ RSpec.describe User, type: :model do
   before do
     @user = FactoryBot.build(:user)
   end
+
+  describe 'ユーザー新規登録' do
+    it"全ての項目が入力されていると登録できる" do
+      expect(@user).to be_valid
+    end
+    
+    it "nicknameが空だと登録できない" do
+      @user.nickname = ""
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Nickname can't be blank")
+    end
+    it "emailが空では登録できない" do
+      @user.email = ""
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Email can't be blank")
+    end
+    it "passeordが空では登録できない" do
+      @user.password = ""
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Password can't be blank")
+    end
+  end
 end
