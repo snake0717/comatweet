44cee6dd967ffd23b62e82653116efd45b7dee13
snake0717 <69613407+snake0717@users.noreply.github.com>
2020-10-07 14:47:53 +0900

    修正

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -4,9 +4,9 @@ class LikesController < ApplicationController
     redirect_to "/tweets/#{@like.tweet.id}"
   end
 
-  def destroy
-    @like = Like.find_by(tweet_id: params[:tweet_id], user_id: current_user.id)
-    @like.destroy
-    redirect_to root_path
-  end
+  # def destroy
+  #   @like = Like.find_by(tweet_id: params[:tweet_id], user_id: current_user.id)
+  #   @like.destroy
+  #   redirect_to root_path
+  # end
 end
